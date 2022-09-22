d2ffad2cda7c8dd56728fb72f8c4fadbd972d33d
Dimitar Panayotov <neznajko@yahoo.com>
2022-09-22 18:50:04 +0300

    :)

--- deps.mk
+++
@@ -1,7 +0,0 @@
-chess.o: chess.cpp node.hpp figure.hpp move.hpp coor.hpp io.hpp cast.hpp
-io.o: io.cpp io.hpp
-coor.o: coor.cpp coor.hpp io.hpp
-move.o: move.cpp figure.hpp move.hpp coor.hpp io.hpp
-figure.o: figure.cpp node.hpp figure.hpp move.hpp coor.hpp io.hpp
-node.o: node.cpp node.hpp figure.hpp move.hpp coor.hpp io.hpp
-cast.o: cast.cpp cast.hpp
